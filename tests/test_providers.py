b6027456bebeaaa47d2b63d1f7b6de2a808a473f
rcattin <rafa.cattin@gmail.com>
2023-06-21 11:22:31 +0200

    :sparkles: add info command

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -2,7 +2,7 @@
 import pytest
 from typer.testing import CliRunner
 
-from thipstercli.commands.providers import check_provider_exists, provider_app
+from thipstercli.commands.providers import check_provider_exists, providers_app
 from thipstercli.config import init_parameters, state
 from thipstercli.helpers import get_auth_provider_class
 
@@ -25,7 +25,7 @@ def config_file_wrong_provider(create_config_file):
 
 def test_list_providers():
     """Test the list command."""
-    result = runner.invoke(provider_app, ['list'])
+    result = runner.invoke(providers_app, ['list'])
     assert result.exit_code == 0
     for provider in providers:
         assert provider in result.stdout.lower()
@@ -33,7 +33,7 @@ def test_list_providers():
 
 def test_info_provider():
     """Test the info command for 'google'."""
-    result = runner.invoke(provider_app, ['info', 'google'])
+    result = runner.invoke(providers_app, ['info', 'google'])
     assert result.exit_code == 0
     assert 'google' in result.stdout.lower()
     assert 'gcloud' in result.stdout.lower()
@@ -41,14 +41,14 @@ def test_info_provider():
 
 def test_info_provider_not_found():
     """Test the info command for wrong provider 'notfound'."""
-    result = runner.invoke(provider_app, ['info', 'notfound'])
+    result = runner.invoke(providers_app, ['info', 'notfound'])
     assert result.exit_code == 1
     assert 'provider notfound not found.' in result.stdout.lower()
 
 
 def test_set_provider():
     """Test the set command for 'google'."""
-    result = runner.invoke(provider_app, ['set', 'google'])
+    result = runner.invoke(providers_app, ['set', 'google'])
     assert result.exit_code == 0
     assert 'google' in result.stdout.lower()
     assert 'provider set to' in result.stdout.lower()
@@ -56,7 +56,7 @@ def test_set_provider():
 
 def test_set_provider_not_found():
     """Test the set command for wrong provider 'notfound'."""
-    result = runner.invoke(provider_app, ['set', 'notfound'])
+    result = runner.invoke(providers_app, ['set', 'notfound'])
     assert result.exit_code == 1
     assert 'provider notfound not found.' in result.stdout.lower()
 
@@ -64,7 +64,7 @@ def test_set_provider_not_found():
 def test_display_provider(config_file):
     """Test the display command for the 'google' provider set in the config file."""
     _ = config_file
-    result = runner.invoke(provider_app, ['display'])
+    result = runner.invoke(providers_app, ['display'])
     assert result.exit_code == 0
     assert 'provider set to' in result.stdout.lower()
     assert 'google' in result.stdout.lower()
@@ -85,12 +85,12 @@ def test_check_provider_exists():
 def test_set_provider_config_file(config_file_wrong_provider):
     """Test if the set command sets the provider in the config file."""
     _ = config_file_wrong_provider
-    runner.invoke(provider_app, ['set', 'google'])
+    runner.invoke(providers_app, ['set', 'google'])
     assert get_config_file().get('auth_provider') == 'google'
 
 
 def test_wrong_provider_config_file(config_file_wrong_provider):
     """Test the behavior of the app when the config file has a wrong provider."""
     _ = config_file_wrong_provider
-    runner.invoke(provider_app, ['--help'])
+    runner.invoke(providers_app, ['--help'])
     assert state.get('auth_provider') is None
